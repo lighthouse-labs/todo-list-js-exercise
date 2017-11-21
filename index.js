66a79e61112e77fa400ab14b2a4cc4c9d745087e
Sam Meech-Ward <sam@meech-ward.com>
2017-11-21 11:11:19 -0800

    Create some test tasks

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function logTaskState(taskIndex) {
   console.log(`${title} has${complete? " " : " not "}been completed`);
 }
 
+newTask("Clean Cat Litter"); // task 0
+newTask("Do Laundry"); // task 1
+
+logTaskState(0); // Clean Cat Litter has not been completed
+completeTask(0);
+logTaskState(0); // Clean Cat Litter has been completed
