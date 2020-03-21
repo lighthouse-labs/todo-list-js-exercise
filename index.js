806e8f67069fecbca7ac787774c13fcc98c406e6
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-03-21 14:26:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
-
+///
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
 function newTask(title) {
