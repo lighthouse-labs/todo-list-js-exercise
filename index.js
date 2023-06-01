ed46b1c424fd54092a364d4251b997fe01513a2a
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-01 08:43:43 -0400

    Step 2 refactor: refactor functions to accept task object instead of array index

--- index.js
+++ index.js
@@ -14,16 +14,14 @@ function newTask(title, description) {
   return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+// Print the state of a task to the console in a nice readable way
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+// Mark a task as complete by reassigning the value of the task object's "complete" property
+function completeTask(task) {
+  task.complete = true;
 }
 
 // DRIVER CODE BELOW
@@ -31,5 +29,8 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-// for now, let's just make sure we see our tasks
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
 console.log(tasks);
