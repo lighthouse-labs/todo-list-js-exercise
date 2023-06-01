4e1089d409ef02b90ad828b0e4b467af64c6187c
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-01 08:52:44 -0400

    Step 3 refactor: bundle functions with task object as object methods

--- index.js
+++ index.js
@@ -9,19 +9,15 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-// Mark a task as complete by reassigning the value of the task object's "complete" property
-function completeTask(task) {
-  task.complete = true;
 }
 
 // DRIVER CODE BELOW
@@ -29,8 +25,8 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
 
 console.log(tasks);
