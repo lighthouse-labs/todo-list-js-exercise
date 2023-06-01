d6f5bead73377a0763fc38c610889362b6183447
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-01 08:55:10 -0400

    Refactor to function expression

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title: title,
     description: description,
@@ -18,7 +18,7 @@ function newTask(title, description) {
     }
   };
   return task;
-}
+};
 
 // DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
